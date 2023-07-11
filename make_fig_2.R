1af261a211fcf16e10ae60da86d77013a67bd1df
Jeremy BORDERIEUX <borderieux@agroparistech.fr>
2023-07-11 15:46:20 +0200

    scripts to create the figures of the occTest manuscript

---
+++ make_fig_2.R
@@ -0,0 +1,192 @@
+#### Figure 2 ####
+library(occTest)
+library(spocc)
+library(sf)
+library(ggplot2)
+
+
+#Load data
+sp_file <- system.file('ext/exampleOccData.csv',package='occTest')
+occ_species_raw <- read.table(file = sp_file,header = T,sep = ',',as.is = T)
+#Load the raster of the environmental variables to the specific region
+ras_env_file <- system.file('ext/AllEnv.tif',package='occTest')
+ras_env <- raster::stack(ras_env_file)
+#Load a high resolution raster of elevation
+ras_dem_file <- system.file('ext/DEM.tif',package='occTest')
+ras_dem <- raster::raster(ras_dem_file)
+#Load a high resolution coastlines
+pol_ctry_file <- system.file('ext/countries.rds',package='occTest')
+pol_ctry <- readRDS(pol_ctry_file)
+
+
+#show default settings
+show_table_Names()
+#modify settings
+my_table_Settings <- occTest::set_tableNames(x.field = 'MAPX',
+                                             y.field = 'MAPY',
+                                             t.field = 'DATE',
+                                             l.field = 'LOCALITYNAME',
+                                             c.field = 'CONTRYRECORD',
+                                             e.field = 'ELEVATION',
+                                             a.field =c('UNCERTAINTY_X_M','UNCERTAINTY_Y_M'))
+
+my_analysis_settings <- set_testTypes(geoenvLowAccuracy = F)
+my_writeout_settings <- set_writeout(output.dir = getwd(),writeAllOutput = T)
+#run workflow
+occ_species_test <- occTest::occTest(sp.name = 'Genus_species',
+                                     sp.table = occ_species_raw,
+                                     r.env =ras_env,
+                                     tableSettings = my_table_Settings,
+                                     analysisSettings = my_analysis_settings,
+                                     writeoutSettings = my_writeout_settings,
+                                     r.dem =   ras_dem,
+                                     ntv.ctry = 'ESP',
+                                     inv.ctry = 'FRA',
+                                     verbose = T)
+
+#modify some analysis table parameters
+my_analysis_settings <- defaultSettings()$analysisSettings
+my_analysis_settings$humanAnalysis$th.human.influence <-50
+my_analysis_settings$rangeAnalysis$countries.shapefile <-pol_ctry
+my_analysis_settings$rangeAnalysis$countryfield.shapefile <- 'ISO'
+my_analysis_settings$geoenvLowAccuracy$doGeoEnvAccuracy <- F
+my_analysis_settings$envOutliers$methodEnvOutliers <- "grubbs"
+
+
+
+occ_species_raw_shorten<-occ_species_raw[occ_species_raw$MAPY>10,]
+
+
+occ_species_test <- occTest::occTest(sp.name = 'Genus_species',
+                                     sp.table = occ_species_raw,
+                                     r.env =ras_env,
+                                     tableSettings = my_table_Settings,
+                                     analysisSettings = my_analysis_settings,
+                                     writeoutSettings = my_writeout_settings,
+                                     r.dem =   ras_dem,
+                                     ntv.ctry = 'ESP',
+                                     inv.ctry = 'FRA',
+                                     verbose = T)
+
+custom_rules<-readRDS(system.file('ext/fieldMetadata.rds',package='occTest'))
+custom_rules$errorThreshold<-rep(0.5,nrow(custom_rules))
+custom_rules[custom_rules$testBlock=="lu","errorThreshold"]<-0.7
+custom_rules[custom_rules$testBlock=="geo","errorThreshold"]<-0.2
+custom_rules[custom_rules$testBlock=="env","errorThreshold"]<-0.5
+custom_rules[custom_rules$testBlock=="time","errorThreshold"]<-1
+
+
+
+occ_species_test_filter<-occFilter(occ_species_test,custom = custom_rules)
+
+occ_species_test_sf<-st_as_sf(occ_species_test[!is.na(occ_species_test$MAPX)&occ_species_test$MAPY>10,],coords = c("MAPX","MAPY"),crs=st_crs(4326))
+
+
+
+pol_ctry_sf<-st_as_sf(pol_ctry)
+
+
+library(raster)
+
+ras_env <- raster::stack(ras_env_file)
+ras_env_dt<-data.frame(xyFromCell(ras_env,1:ncell(ras_env)))
+ras_env_dt$Env<-getValues(ras_env$AllEnv_1)
+
+hii<-my_analysis_settings$humanDetection$ras.hii
+hii<-crop(hii,ras_env)
+hii_dt<-data.frame(xyFromCell(hii,1:ncell(hii)))
+hii_dt$Hii<-getValues(hii)
+
+
+size_big_point<-1.7
+size_small_point<-1.45
+
+plot_1<-ggplot(pol_ctry_sf)+
+  geom_sf(fill="grey90")+
+  theme_bw()+
+  geom_sf(data=occ_species_test_sf,size=size_big_point)+
+  #  geom_sf(data=occ_species_test_sf,aes(color=geoenvLowAccuracy_score),size=size_small_point)+  # no geo accuracy test done, but coul be showed too ? 
+  labs(x="",y="",title="Original dataset")+
+  coord_sf(expand = F)
+
+occ_species_test_sf$Env_score<-as.factor(round(occ_species_test_sf$envOutliers_score,2))
+
+plot_env<-ggplot(pol_ctry_sf)+
+  geom_raster(data=ras_env_dt,aes(x=x,y=y,fill=Env))+
+  geom_sf(fill=NA)+
+  theme_bw()+
+  geom_sf(data=occ_species_test_sf,color="black",size=size_big_point)+
+  geom_sf(data=occ_species_test_sf,aes(color=Env_score),size=size_small_point)+
+  scale_fill_viridis_c(na.value = "transparent")+
+  scale_color_manual(values = c("skyblue2","tan1","tomato4"))+
+  labs(x="",y="",title="Environmental outlier tests")+
+  theme(legend.position = "bottom")+
+  coord_sf(expand = F)+
+  guides(fill=guide_colorbar(title.position = "top", title.hjust = 0.5),color = guide_legend(title.position = "top", title.hjust = 0.5,label.position = "bottom",override.aes=list(size=3)))
+#scale_fill_gradient2(na.value = "transparent",low="blanchedalmond",mid="lightsteelblue",high = "lightsalmon",midpoint = 7,limits=c(0,15),oob=scales::squish)
+
+occ_species_test_sf$Hii_score<-as.factor(occ_species_test_sf$HumanDetection_score)
+library(data.table)
+hii_dt<-data.table(hii_dt)
+missing_y<-hii_dt[,sum(is.na(Hii)),by=y][order(-V1),][1,y]
+one_pixel_north<-hii_dt[,sum(is.na(Hii)),by=y][order(abs(y-40.72903)),][2,y]
+
+
+hii_dt[y==missing_y,Hii:=hii_dt[y==one_pixel_north,Hii]]
+hii_dt[y==missing_y,]
+
+hii_dt[y==one_pixel_north,]
+
+plotly::ggplotly(plot_hii)
+
+plot_hii<-ggplot(pol_ctry_sf)+
+  geom_tile(data=hii_dt,aes(x=x,y=y,fill=Hii))+
+  geom_sf(fill=NA)+
+  theme_bw()+
+  geom_sf(data=occ_species_test_sf,color="black",size=size_big_point)+
+  geom_sf(data=occ_species_test_sf,aes(color=Hii_score),size=size_small_point)+
+  scale_fill_gradient2(na.value = "transparent",low="mintcream",mid="seashell3",high = "orangered3",midpoint = 20)+
+  scale_color_manual(values = c("skyblue2","tan1","tomato4"))+
+  labs(x="",y="",title="Human influence tests")+
+  theme(legend.position = "bottom")+
+  coord_sf(expand = F)+ guides(fill=guide_colorbar(title.position = "top", title.hjust = 0.5,order=1),color = guide_legend(title.position = "top", title.hjust = 0.5,label.position = "bottom",override.aes=list(size=3)))
+
+
+
+occ_species_test_sf$Filtered<-!occ_species_test_sf$ID_GRAFIC%in%occ_species_test_filter$filteredDataset$ID_GRAFIC
+
+plot_filter<-ggplot(pol_ctry_sf)+
+  geom_sf(fill="grey90")+
+  theme_bw()+
+  #geom_sf(data=occ_species_test_sf,color="black",size=2)+
+  geom_sf(data=occ_species_test_sf[occ_species_test_sf$Filtered==T,],aes(color="Filtered"),size=size_small_point,shape=4, stroke = 2)+
+  geom_sf(data=occ_species_test_sf[occ_species_test_sf$Filtered==F,],aes(color="Kept"),size=size_big_point,shape=16)+
+  scale_color_manual(values=c("red3","royalblue1"))+
+  guides(colour =guide_legend(override.aes = list(shape=16) ))+
+  labs(x="",y="",color="Occurrences",title="Filtered occurrences")+
+  theme(legend.position = "bottom")+scale_x_continuous()+
+  coord_sf(expand = F)
+
+occ_species_test_sf$Geo_score<-as.factor(occ_species_test_sf$geoOutliers_score)
+
+plot_geo<-ggplot(pol_ctry_sf)+
+  geom_sf(fill="grey90")+
+  theme_bw()+
+  geom_sf(data=occ_species_test_sf,color="black",size=size_big_point)+
+  geom_sf(data=occ_species_test_sf,aes(color=Geo_score),size=size_small_point)+
+  scale_color_manual(values = c("skyblue2","tan1","tomato4"))+
+  guides(fill = guide_colourbar(order = 1),color = guide_legend(order = 2,override.aes=list(size=3)))+
+  labs(x="",y="",title="Geographic outlier tests")+
+  theme(legend.position = "bottom")+
+  coord_sf(expand = F)+guides(color = guide_legend(title.position = "top", title.hjust = 0.5,label.position = "bottom",override.aes=list(size=3))) 
+
+plot_geo
+
+library(patchwork)
+
+results_arrange<-plot_1+plot_geo+plot_hii+plot_env+ plot_annotation(tag_levels = 'a',tag_suffix = ')')& theme(plot.tag = element_text(face="bold"))
+
+ggsave("Figure_manuscript_ex_2.png",results_arrange,units = "mm",width = 180,height = 190,dpi=300,scale=1.4, type = "cairo") 
+ggsave("Figure_manuscript_ex_2.pdf",results_arrange,units = "mm",width = 180,height = 180,dpi=300,scale=1.4) 
+
+plot(occ_species_test,occ_species_test_filter)
---
+++ make_fig_3.R
@@ -0,0 +1,133 @@
+
+#### figure 4 ####
+
+library(occTest)
+library(spocc)
+# Getting some tree occurrences
+df_qupe <- occ(query ='Quercus petraea',limit = 1000)
+occ_data_qupe <-occ2df(df_qupe)
+names (occ_data_qupe)[2] <- 'decimalLongitude'
+names (occ_data_qupe)[3] <- 'decimalLatitude'
+
+# downloading environental data
+library (raster)
+renv = raster::getData(name='worldclim',var='bio', res=10,path = tempdir())
+
+# running occTest
+out_qupe = occTest(sp.name='Quercus petraea',sp.table = occ_data_qupe ,r.env = renv,verbose = F)
+# running a strict filtering process
+out_filtered_qupe= occFilter (df = out_qupe , errorAcceptance = 'strict')
+
+custom_rules<-readRDS(system.file('ext/fieldMetadata.rds',package='occTest'))
+custom_rules$errorThreshold<-rep(0.5,nrow(custom_rules))
+custom_rules[custom_rules$testBlock=="lu","errorThreshold"]<-0.7
+custom_rules[custom_rules$testBlock=="geo","errorThreshold"]<-0.2
+
+
+out_filtered_qupe = occFilter(df = out_filtered_qupe,custom=custom_rules)
+
+
+list_of_plots<-plot(x = out_qupe , occFilter_list = out_filtered_qupe, show_plot = F)
+list_of_plots
+
+
+
+
+export_2_2<-ggarrange(plotlist = list_of_plots[1:4],nrow=2,ncol=2,align = "hv",labels=c("a)","b)","c)","d)"))
+
+
+ggsave("Figure_manuscript_2.jpg",export_2_2,units = "mm",width = 180,height = 130,dpi=300,scale=1.5) # psme
+# 
+# 
+# results_arrange<-ggarrange(plotlist = list(plot_1,plot_env,plot_hii,plot_filter),ncol=2,nrow=2,align = "v",labels=c("a)","b)","c)","d)"))
+# results_arrange<-ggarrange(plotlist = list(plot_1,plot_env),ncol=1,nrow=2,align = "v",labels=c("a)","b)"))
+# 
+# occ_species_test$tot_score<-rowSums( occ_species_test[,grep("_score",colnames(occ_species_test),value=T)],na.rm = T)
+# occ_species_test_sf$tot_score<-rowSums( occ_species_test[,grep("_score",colnames(occ_species_test),value=T)],na.rm = T)
+# 
+
+
+library(ggpubr)
+#load packages
+library(occTest)
+library(spocc)
+library(sf)
+
+sp_to_test<-"Abies alba"
+sp_to_test<-"Fagus sylvatica"
+sp_to_test<-"Quercus petraea"
+
+
+df <- occ(query = sp_to_test,limit = 1000)
+occ.data <-occ2df(df)
+
+#we change the names here but we could just change it in the tableSettings
+names (occ.data)[2] <- 'decimalLongitude'
+names (occ.data)[3] <- 'decimalLatitude'
+
+#load needed environmental data
+library (raster)
+renv = raster::getData(name='worldclim',var='bio', res=10,path = tempdir())
+
+#test occurrence
+
+
+outDougFir = occTest(sp.name=sp_to_test,sp.table = occ.data,r.env = renv,verbose = F)
+
+
+
+#filter data
+outDougFirSelec = occFilter(df = outDougFir,errorAcceptance = 'relaxed')
+
+custom_rules<-readRDS(system.file('ext/fieldMetadata.rds',package='occTest'))
+
+custom_rules$errorThreshold<-rep(0.5,nrow(custom_rules))
+custom_rules[custom_rules$testBlock=="lu","errorThreshold"]<-0.6
+custom_rules[custom_rules$testBlock=="geo","errorThreshold"]<-0.1
+custom_rules[custom_rules$testBlock=="env","errorThreshold"]<-0.2
+custom_rules[custom_rules$testBlock=="time","errorThreshold"]<-1
+
+
+outDougFirSelec = occFilter(df = outDougFir,custom=custom_rules)
+
+plot(outDougFir,outDougFirSelec)
+#show filtered dataset
+nrow (outDougFir)
+nrow (outDougFirSelec[["filteredDataset"]])
+
+
+outDougFirSelecstrict = occFilter (df = outDougFir, errorAcceptance = 'strict')
+
+
+list_of_plots<-plot(outDougFir , occFilter_list = outDougFirSelec, show_plot = F)
+
+
+
+sf_doug<-st_as_sf(outDougFir,coords=c("decimalLongitude",'decimalLatitude'),crs=st_crs(4326))
+
+list_of_plots
+
+crop_hii<-sets$analysisSettings$humanDetection$ras.hii
+crop_hii<-crop(crop_hii,bbox(as_Spatial( sf_doug)))
+crop_hii_dt<-data.frame(xyFromCell(crop_hii,1:ncell(crop_hii)))
+crop_hii_dt$hii<-getValues(crop_hii$hii3)
+
+list_of_plots[[3]]+geom_raster(data=crop_hii_dt,aes(x=x,y=y,fill=hii),alpha=0.7)+scale_fill_gradientn(breaks=c(0,20,40,60),colors=c("grey90","grey70","violetred3"),na.value =NA )
+
+
+first_filter<-list_of_plots[[1]]
+filtertype_3<-ggarrange(plotlist = list_of_plots[2:4],nrow=3,ncol=1,common.legend = T,legend="bottom")
+
+export_4_1<-ggarrange(first_filter,filtertype_3,nrow=2,heights = c(1.32,3),align = "v")
+
+ggsave("Figure_manuscript.jpg",export_4_1,units = "mm",width = 180,height = 360,dpi=300)
+
+
+export_2_2<-ggarrange(plotlist = list_of_plots[1:4],nrow=2,ncol=2,align = "hv",labels=c("a)","b)","c)","d)"))
+
+
+ggsave("Figure_manuscript_2.jpg",export_2_2,units = "mm",width = 180,height = 130,dpi=300,scale=1.5) # psme
+
+ggsave("Figure_manuscript_2.jpg",export_2_2,units = "mm",width = 180,height = 160,dpi=300,scale=1.5) # qupe
+ggsave("Figure_manuscript_3.jpg",export_2_2,units = "mm",width = 180,height = 170,dpi=300,scale=1.5) # qupe
+ggsave("Figure_manuscript_4.jpg",export_2_2,units = "mm",width = 180,height = 145,dpi=300,scale=1.5) # qupe
